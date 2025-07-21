9e9148de3fe1da9e0077c373b5b758cb5c141a1b
thmastin <thmastin@gmail.com>
2025-07-20 21:24:59 -0400

    Add user login and register fucntionality

--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"errors"
 	"fmt"
 	"os"
+	"time"
 
+	"github.com/google/uuid"
+	_ "github.com/lib/pq"
 	"github.com/thmastin/gator/internal/config"
+	"github.com/thmastin/gator/internal/database"
 )
 
 func main() {
@@ -13,14 +19,24 @@ func main() {
 	if err != nil {
 		fmt.Println(err)
 	}
+
+	db, err := sql.Open("postgres", cfg.DbURL)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	dbQueries := database.New(db)
+
 	var currentState state
-	currentState.pointer = &cfg
+	currentState.db = dbQueries
+	currentState.cfg = &cfg
 
 	cmds := commands{
 		handlers: make(map[string]func(*state, command) error),
 	}
 
 	cmds.register("login", handlerLogin)
+	cmds.register("register", handlerRegister)
 
 	cliInput := os.Args
 	if len(cliInput) < 2 {
@@ -45,7 +61,8 @@ func main() {
 }
 
 type state struct {
-	pointer *config.Config
+	db  *database.Queries
+	cfg *config.Config
 }
 
 type command struct {
@@ -57,12 +74,49 @@ func handlerLogin(s *state, cmd command) error {
 	if len(cmd.arguments) < 1 {
 		return errors.New("you must enter a username")
 	}
-	err := config.SetUser(*s.pointer, cmd.arguments[0])
+	_, err := s.db.GetUser(context.Background(), cmd.arguments[0])
+	if err != nil {
+		fmt.Println("user not found")
+		os.Exit(1)
+	}
+
+	err = config.SetUser(*s.cfg, cmd.arguments[0])
 	if err != nil {
 		return err
 	}
 	fmt.Println("User has been set")
 	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.arguments) < 1 {
+		return errors.New("you must enter a username")
+	}
+
+	name := cmd.arguments[0]
+
+	user, err := s.db.CreateUser(context.Background(), database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      name,
+	})
+
+	if err != nil {
+		fmt.Println("Error creating user:", err)
+		os.Exit(1)
+	}
+
+	err = config.SetUser(*s.cfg, cmd.arguments[0])
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("User created successfully!")
+	fmt.Printf("User data: %+v\n", user)
+
+	return nil
+
 }
 
 type commands struct {
